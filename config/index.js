772835afee968b38d06c8a1577c47d6d6e1af11c
Dustin L. Dodson <dustinleedodson@gmail.com>
2020-08-31 17:56:43 -0700

    Updates

--- config/index.js
+++ config/index.js
@@ -1,5 +1,31 @@
+'use strict';
+
 const convict = require('convict');
 
-const conf = convict();
+const conf = convict({
+    credentials: {
+        clientId: {
+            doc: 'The Discord Client ID',
+            format: 'Number',
+            env: 'CLIENT_ID',
+            default: '',
+            arg: 'i',
+        },
+        clientSecret: {
+            doc: 'The Discord Client Secret',
+            format: 'String',
+            env: 'CLIENT_SECRET',
+            default: '',
+            arg: 's',
+        },
+        token: {
+            doc: 'The Discord Bot Token',
+            format: 'String',
+            env: 'BOT_TOKEN',
+            default: '',
+            arg: 't',
+        },
+    },
+});
 
 module.exports = conf;
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 
-module.exports = {};
+const Discord = require('discord.js');
+const log = require('@fizzygalacticus/colored-fancy-log');
+
+const config = require('./config');
+
+const client = new Discord.Client();
+
+client.on('ready', () => log.log('Bonesaw is ready!'));
+
+client.on('message', (message) => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+
+    log.log('Received message:', message);
+});
+
+client.login(config.get('credentials.token')).catch((e) => log.error(e));
