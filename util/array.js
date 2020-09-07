97f23f0b79d6b8abdf8b4e1b96176a8f1c7d114c
Dustin L. Dodson <dustinleedodson@gmail.com>
2020-09-06 18:14:08 -0700

    Roast me

--- util/array.js
+++ util/array.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const getRandomElement = (arr = [], previousIndex) => {
+const getRandomElement = (arr = [], previousIndex = []) => {
+    const previousIsArray = Array.isArray(previousIndex);
     let index;
+
+    if (previousIsArray && previousIndex.length === arr.length) {
+        previousIndex = [];
+    }
 
     do {
         index = Math.floor(Math.random() * arr.length);
-    } while (index === previousIndex);
+    } while (Array.isArray(previousIndex) ? previousIndex.includes(index) : index === previousIndex);
 
     const el = arr[index];
 
-    return { el, index };
+    return { el, index: Number.isInteger(previousIndex) ? index : [...previousIndex, index] };
 };
 
 module.exports = { getRandomElement };
