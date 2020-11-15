00e053fd5c10e4aca1532561c62aaaa8c88a3966
Dustin L. Dodson <dustinleedodson@gmail.com>
2020-11-14 16:56:37 -0800

    Only make array responses trigger from start of line, fix typos/dupes

--- tests/unit/lib/events/message/fun.js
+++ tests/unit/lib/events/message/fun.js
@@ -2,7 +2,9 @@
 
 const { expect } = require('chai');
 
-const { ping, foo, fizz } = require('../../../../../lib/events/message/fun');
+const { parseMatch } = require('../../../../../util/regex');
+
+const { ping, foo, fizz, roast, compliment, comeOn, pickUp } = require('../../../../../lib/events/message/fun');
 
 describe('fun', () => {
     describe('ping', () => {
@@ -20,6 +22,54 @@ describe('fun', () => {
     describe('fizz', () => {
         it(`should respond with 'buzz'`, () => {
             fizz.process(null, { reply: res => expect(res).to.equal('buzz') });
+        });
+    });
+
+    describe(`roast`, () => {
+        it(`should only be triggered with 'roast' at the beginning of a message`, () => {
+            let matches = parseMatch(roast.match, 'roast Leeroy');
+
+            expect(matches).to.have.lengthOf(2);
+
+            matches = parseMatch(roast.match, 'This pot roast is great!');
+
+            expect(matches).to.have.lengthOf(0);
+        });
+    });
+
+    describe(`compliment`, () => {
+        it(`should only be triggered with 'compliment' at the beginning of a message`, () => {
+            let matches = parseMatch(compliment.match, 'compliment Leeroy');
+
+            expect(matches).to.have.lengthOf(2);
+
+            matches = parseMatch(compliment.match, `I don't know how to take a compliment`);
+
+            expect(matches).to.have.lengthOf(0);
+        });
+    });
+
+    describe(`comeOn`, () => {
+        it(`should only be triggered with 'come on to' at the beginning of a message`, () => {
+            let matches = parseMatch(comeOn.match, 'come on to Leeroy');
+
+            expect(matches).to.have.lengthOf(2);
+
+            matches = parseMatch(comeOn.match, `I don't like to come on to strangers`);
+
+            expect(matches).to.have.lengthOf(0);
+        });
+    });
+
+    describe(`pickUp`, () => {
+        it(`should only be triggered with 'pick up' at the beginning of a message`, () => {
+            let matches = parseMatch(pickUp.match, 'pick up Leeroy');
+
+            expect(matches).to.have.lengthOf(2);
+
+            matches = parseMatch(pickUp.match, `I like to pick up things and put them down`);
+
+            expect(matches).to.have.lengthOf(0);
         });
     });
 });
